43807f49ea3d5bc47fef51ee520a0dd2a4cd6a70
matti <eng.muhammad-com@hotmail.com>
2024-12-29 05:56:47 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pbCanvas = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            gameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pbCanvas).BeginInit();
+            SuspendLayout();
+            // 
+            // pbCanvas
+            // 
+            pbCanvas.BackColor = Color.Yellow;
+            pbCanvas.Location = new Point(13, 13);
+            pbCanvas.Name = "pbCanvas";
+            pbCanvas.Size = new Size(541, 560);
+            pbCanvas.TabIndex = 0;
+            pbCanvas.TabStop = false;
+            pbCanvas.Click += pictureBox1_Click;
+            pbCanvas.Paint += updateGraphics;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label1.Location = new Point(614, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 38);
+            label1.TabIndex = 1;
+            label1.Text = "Score:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label2.Location = new Point(717, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 38);
+            label2.TabIndex = 2;
+            label2.Text = "00";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ControlText;
+            label3.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label3.ForeColor = Color.Yellow;
+            label3.Location = new Point(215, 256);
+            label3.Name = "label3";
+            label3.Size = new Size(128, 38);
+            label3.TabIndex = 3;
+            label3.Text = "End Text";
+            label3.Click += label3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoValidate = AutoValidate.EnablePreventFocusChange;
+            ClientSize = new Size(903, 614);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pbCanvas);
+            Name = "Form1";
+            Text = "Snake Game";
+            KeyDown += keyisdown;
+            KeyUp += keyisup;
+            ((System.ComponentModel.ISupportInitialize)pbCanvas).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+
+        private PictureBox pbCanvas;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
